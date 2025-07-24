5bfcd635d4bc78837434d29b510d80fc20abd9d6
unknown <valododa123@gmail.com>
2025-07-24 11:45:22 +0200

    Revert a mistake

---
+++ my-ts-project/app.ts
@@ -0,0 +1,16 @@
+function greet(name : string): string {
+    return `Hello ${name}, how was your day ?`;
+}
+
+const greeting: string = greet("Valentin");
+
+console.log(greeting);
+
+function userData(name :string , age :number): void {
+    console.log(`${name} is ${age} years old`)
+}
+
+userData("Valentin", 21);
+// This comment will be used for to test tsc --watch. What tsc -watch does? With tsc watch is not anymore necessary to run everytime the tch command for to update our code, because now it happen automatic
+
+// writing ts. But because the ts is not understatable we should command "tsc" so the typescript cli to turns it to js files and this js file we can run it in node or use it in our script html

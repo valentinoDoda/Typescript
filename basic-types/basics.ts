711a0fdc65d2aebf3383aaf2005e25d4a9a7018f
unknown <valododa123@gmail.com>
2025-07-24 13:06:05 +0200

    Typing some basic types and set up the ts env

---
+++ basic-types/basics.ts
@@ -0,0 +1,15 @@
+const name1 :string = "Valentin" // type string
+const age :number = 12 // type number
+const isAdult :boolean = true //type boolean
+const programmingLangs: string[] = ['js', 'astro', 'vue']; // array of strings
+const friends: Array<string> = ["a","b"]; // alternative way
+const anyType :any = "A" // can take any type of value
+
+console.log(
+    name1,
+    age,
+    isAdult,
+    programmingLangs,
+    friends,
+    anyType
+)

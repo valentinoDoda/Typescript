b9abf8c939bc0d352ec0ccf033df06d4e5d59a95
unknown <valododa123@gmail.com>
2025-07-24 11:02:48 +0200

    tsc --watch for automatic updates

--- my-ts-project/dist/app.js
+++ my-ts-project/dist/app.js
@@ -4,4 +4,9 @@ function greet(name) {
 }
 const greeting = greet("Valentin");
 console.log(greeting);
+function userData(name, age) {
+    console.log(`${name} is ${age} years old`);
+}
+userData("Valentin", 21);
+// This comment will be used for to test tsc --watch. What tsc -watch does? With tsc watch is not anymore necessary to run everytime the tch command for to update our code, because now it happen automatic
 // writing ts. But because the ts is not understatable we should command "tsc" so the typescript cli to turns it to js files and this js file we can run it in node or use it in our script html

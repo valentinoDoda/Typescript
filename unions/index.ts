f1461dc5c159eea6feb7375db9e668c9277f4db6
unknown <valododa123@gmail.com>
2025-07-26 11:06:55 +0200

    instance of

--- unions/index.ts
+++ unions/index.ts
@@ -73,3 +73,22 @@ const handleResponse = (response: APIResponse) => {
     throw new Error(response.error);
   }
 };
+
+
+const somethingDangerous = () => {
+  if (Math.random() > 0.5) {
+    throw new Error("Something went wrong");
+  }
+
+  return "all good";
+};
+
+try {
+  somethingDangerous();
+} catch (error) {
+  // How do we change this code to make it
+  // not show a red squiggly?
+  if (error instanceof Error) {
+    console.error(error.message);
+  }
+}

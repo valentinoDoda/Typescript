ae649d942d2a74e83a92b692236d31a83a12eb23
unknown <valododa123@gmail.com>
2025-07-26 14:28:09 +0200

    Unions of type

--- unions/index.ts
+++ unions/index.ts
@@ -92,3 +92,36 @@ try {
     console.error(error.message);
   }
 }
+
+
+type User = {
+  id: string;
+};
+type SuccessResponse = [
+  response: "success",
+  User[]
+]
+
+type ErrorResponse = [
+  response: "error",
+  string
+]
+type ApiResponse = SuccessResponse | ErrorResponse;
+
+async function fetchData(): Promise<ApiResponse> {
+  try {
+    const response = await fetch("https://api.example.com/data");
+    if (!response.ok) {
+      return [
+        "error",
+        // Imagine some improved error handling here
+        "An error occurred",
+      ];
+    }
+
+    const data = await response.json();
+    return ["success", data];
+  } catch (error) {
+    return ["error", "An error occurred"];
+  }
+}

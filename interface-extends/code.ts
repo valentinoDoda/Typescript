e76cd0df0b3046f56c1219638f0f1c1a04a13c48
unknown <valododa123@gmail.com>
2025-07-27 11:40:55 +0200

    Chaning Multiply interface

--- interface-extends/code.ts
+++ interface-extends/code.ts
@@ -12,3 +12,26 @@ interface Product extends BaseEntity  {
   name: string;
   price: number;
 }
+
+
+interface UserPart  {
+  name: string;
+  age: number;
+};
+
+interface UserPart2  {
+  phone: string;
+};
+
+interface User2 extends UserPart , UserPart2 {
+  id: string
+}
+
+const user: User2 = {
+  id: "1",
+  name: "John",
+  age: 20,
+  phone: "123456789",
+};
+
+

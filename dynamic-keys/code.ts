3956aa00f6756a64b5bb04d2f31cb9064e99ce63
unknown <valododa123@gmail.com>
2025-07-27 21:24:46 +0200

    Challenge

--- dynamic-keys/code.ts
+++ dynamic-keys/code.ts
@@ -13,3 +13,12 @@ const scores: Record<string, number> = {};
 scores.math = 95;
 scores.english = 90;
 scores.science = 85;
+
+
+//Challenge: Your challenge is to determine the correct typing for this Scores interface that will specify the default keys, as well as allow for any other string key to be added to it.
+
+// Currently Scores uses an interface, but you can change its type if you like.
+
+// Once you've made your modifications, you should be able to assign new properties to the scores object, and the @ts-expect-error error will go away because science will be required but missing.
+
+

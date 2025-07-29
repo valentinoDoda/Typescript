2d9f2698816b0d10920c6c8bf9c35eb00bc9001b
unknown <valododa123@gmail.com>
2025-07-29 14:14:48 +0200

    About Readonly

--- mutability/code.ts
+++ mutability/code.ts
@@ -12,3 +12,18 @@ type ButtonAttributes = {
 const buttonAttributes: ButtonAttributes = {
   type,
 };
+
+
+type User = {
+  id: number;
+  name: string;
+  age: number;
+};
+
+const updateUser = (user: User) => {
+  user.name = "Jane Doe";
+  user.age = 30;
+
+  // @ts-expect-error Should not be able to modify readonly
+  user.id = 1;
+};

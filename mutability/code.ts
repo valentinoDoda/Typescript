c572ffac315cfb37df7f6d6ecc798bdc5fd65fe8
unknown <valododa123@gmail.com>
2025-07-29 14:31:38 +0200

    Problem

--- mutability/code.ts
+++ mutability/code.ts
@@ -56,3 +56,14 @@ const handleSearchParams = (search: Readonly<SearchParams>) => {
 
 
 
+function printNames(names: string[]) {
+  for (const name of names) {
+    console.log(name);
+  }
+
+  // @ts-expect-error
+  names.push("John");
+
+  // @ts-expect-error
+  names[0] = "Billy";
+}

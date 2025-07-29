6954b1107a34f2453726ca3b24289f1de164cf04
unknown <valododa123@gmail.com>
2025-07-29 14:17:15 +0200

    The problem

--- mutability/code.ts
+++ mutability/code.ts
@@ -27,3 +27,31 @@ const updateUser = (user: User) => {
   // @ts-expect-error Should not be able to modify readonly
   user.id = 1;
 };
+
+
+type SearchParams = {
+  q?: string;
+  page?: number;
+  pageSize?: number;
+  sort?: string;
+  order?: "asc" | "desc";
+};
+
+const handleSearchParams = (search: SearchParams) => {
+  // Do something with the search params
+
+  // @ts-expect-error Should not be able to modify readonly
+  search.q = "test";
+
+  // @ts-expect-error Should not be able to modify readonly
+  search.page = 1;
+
+  // @ts-expect-error Should not be able to modify readonly
+  search.pageSize = 10;
+
+  // @ts-expect-error Should not be able to modify readonly
+  search.sort = "name";
+
+  // @ts-expect-error Should not be able to modify readonly
+  search.order = "asc";
+};

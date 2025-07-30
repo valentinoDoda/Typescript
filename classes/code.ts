c98e850187df7cd37fd369290e226fdac3b4ebd3
unknown <valododa123@gmail.com>
2025-07-30 10:04:52 +0200

    Add a move method task

--- classes/code.ts
+++ classes/code.ts
@@ -1,16 +1,25 @@
+// class CanvasNode {
+//     readonly x: number
+//     readonly y:  number
+//     constructor(){
+//         this.x = 14;
+//         this.y = 10; 
+//     }
+// }
+
+
+// const canvasNode = new CanvasNode()
+// // @ts-expect-error Property is readonly
+//   canvasNode.x = 10;
+
+//   // @ts-expect-error Property is readonly
+//   canvasNode.y = 20;
+
+
 class CanvasNode {
-    readonly x: number
-    readonly y:  number
-    constructor(){
-        this.x = 14;
-        this.y = 10; 
-    }
+  x = 0;
+  y = 0;
 }
 
 
-const canvasNode = new CanvasNode()
-// @ts-expect-error Property is readonly
-  canvasNode.x = 10;
-
-  // @ts-expect-error Property is readonly
-  canvasNode.y = 20;
+  canvasNode.move(10, 20);

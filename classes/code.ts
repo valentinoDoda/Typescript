89f2bba6f0ffbffef4ecd014a3262e369c0ebd4f
unknown <valododa123@gmail.com>
2025-07-30 11:24:53 +0200

    Starter for the new challenge

--- classes/code.ts
+++ classes/code.ts
@@ -16,30 +16,45 @@
 //   canvasNode.y = 20;
 
 
-type Canvas = {
-    x: number,
-    y:number 
-}
+// type Canvas = {
+//     x: number,
+//     y:number 
+// }
 
-class CanvasNode {
-  x = 0;
-  y = 0;
-  constructor( obj?:Canvas){
-    if(obj){
-        this.x = obj.x;
-        this.y = obj.y;
-    }
-  }
-    move(x: number, y: number){
-this.x = x;
-this.y = y;
-}
-}
+// class CanvasNode {
+//   x = 0;
+//   y = 0;
+//   constructor( obj?:Canvas){
+//     if(obj){
+//         this.x = obj.x;
+//         this.y = obj.y;
+//     }
+//   }
+//     move(x: number, y: number){
+// this.x = x;
+// this.y = y;
+// }
+// }
 
 
-const canvasNode = new CanvasNode();
+// const canvasNode = new CanvasNode();
 
-canvasNode.move(10, 20);
+// canvasNode.move(10, 20);
 
 // Your challenge is to modify the CanvasNode class so that it can receive
 //  an optional initial position. This will require you to add a constructor that can accept an argument.
+
+class CanvasNode {
+  x: number;
+  y: number;
+
+  constructor(position?: { x: number; y: number }) {
+    this.x = position?.x ?? 0;
+    this.y = position?.y ?? 0;
+  }
+
+  move(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}

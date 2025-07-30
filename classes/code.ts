eb1b5f2b0b804d7cf2bc88bcd319472745c0f659
unknown <valododa123@gmail.com>
2025-07-30 09:59:22 +0200

    ts with classes

--- classes/code.ts
+++ classes/code.ts
@@ -1,7 +1,14 @@
-class CanvasNode {}
+class CanvasNode {
+    readonly x: number
+    readonly y:  number
+    constructor(){
+        this.x = 14;
+        this.y = 10; 
+    }
+}
 
 
-const canvasNode = new CanvasNode
+const canvasNode = new CanvasNode()
 // @ts-expect-error Property is readonly
   canvasNode.x = 10;
 

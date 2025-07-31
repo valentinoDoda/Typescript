b5ee269857d8b76b7c0d6e493fd31d66311e720e
unknown <valododa123@gmail.com>
2025-07-31 15:19:52 +0200

    Creating Type from a variable with type of and key of first way

--- deriving-types/code.ts
+++ deriving-types/code.ts
@@ -24,3 +24,21 @@ const inputs: Record<
     label: "Password",
   },
 };
+
+
+const configurations = {
+  development: {
+    apiBaseUrl: "http://localhost:8080",
+    timeout: 5000,
+  },
+  production: {
+    apiBaseUrl: "https://api.example.com",
+    timeout: 10000,
+  },
+  staging: {
+    apiBaseUrl: "https://staging.example.com",
+    timeout: 8000,
+  },
+};
+
+type Environment = keyof typeof configurations;

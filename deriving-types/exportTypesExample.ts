8468904700c4453ebc4402591877904b416a92eb
unknown <valododa123@gmail.com>
2025-07-31 17:35:28 +0200

    Creating a Logger Type and a Logger variable

---
+++ deriving-types/exportTypesExample.ts
@@ -0,0 +1,8 @@
+export const Logger = {
+  log: (message: string) => console.log(message),
+  info: (message: string) => console.info(`INFO: ${message}`),
+  warn: (message: string) => console.warn(`WARNING: ${message}`),
+  error: (message: string) => console.error(`ERROR: ${message}`),
+};
+
+export type Logger = typeof Logger;

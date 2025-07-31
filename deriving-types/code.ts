bc41483774ab302765ce73f4e1340847bdd5fa13
unknown <valododa123@gmail.com>
2025-07-31 16:47:18 +0200

    Commenting the code

--- deriving-types/code.ts
+++ deriving-types/code.ts
@@ -1,63 +1,63 @@
-interface FormValues {
-  name: string;
-  email: string;
-  password: string;
-}
+// interface FormValues {
+//   name: string;
+//   email: string;
+//   password: string;
+// }
 
-const inputs: Record<
-  keyof FormValues,
-  {
-    initialValue: string;
-    label: string;
-  }
-> = {
-  name: {
-    initialValue: "",
-    label: "Name",
-  },
-  email: {
-    initialValue: "",
-    label: "Email",
-  },
-  password: {
-    initialValue: "",
-    label: "Password",
-  },
-};
+// const inputs: Record<
+//   keyof FormValues,
+//   {
+//     initialValue: string;
+//     label: string;
+//   }
+// > = {
+//   name: {
+//     initialValue: "",
+//     label: "Name",
+//   },
+//   email: {
+//     initialValue: "",
+//     label: "Email",
+//   },
+//   password: {
+//     initialValue: "",
+//     label: "Password",
+//   },
+// };
 
 
-const configurations = {
-  development: {
-    apiBaseUrl: "http://localhost:8080",
-    timeout: 5000,
-  },
-  production: {
-    apiBaseUrl: "https://api.example.com",
-    timeout: 10000,
-  },
-  staging: {
-    apiBaseUrl: "https://staging.example.com",
-    timeout: 8000,
-  },
-};
+// const configurations = {
+//   development: {
+//     apiBaseUrl: "http://localhost:8080",
+//     timeout: 5000,
+//   },
+//   production: {
+//     apiBaseUrl: "https://api.example.com",
+//     timeout: 10000,
+//   },
+//   staging: {
+//     apiBaseUrl: "https://staging.example.com",
+//     timeout: 8000,
+//   },
+// };
 
-type Configuration = typeof configurations
+// type Configuration = typeof configurations
 
-type Environment = keyof Configuration;
+// type Environment = keyof Configuration;
 
-class CanvasNode {
-  x = 0;
-  y = 0;
+// class CanvasNode {
+//   x = 0;
+//   y = 0;
 
-  move(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-}
+//   move(x: number, y: number) {
+//     this.x = x;
+//     this.y = y;
+//   }
+// }
 
-const positionFromCanvasNode = (node: CanvasNode) => {
-  return {
-    x: node.x,
-    y: node.y,
-  };
-};
+// const positionFromCanvasNode = (node: CanvasNode) => {
+//   return {
+//     x: node.x,
+//     y: node.y,
+//   };
+// };

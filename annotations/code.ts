1c15cd2d78558cf41efe82f9989bada87111798c
unknown <valododa123@gmail.com>
2025-08-05 15:46:27 +0200

    The solution with as keyword

--- annotations/code.ts
+++ annotations/code.ts
@@ -15,7 +15,7 @@ const isProblemOrSolution = (filename: string): boolean => {
 
 const handleFormData = (e: SubmitEvent) => {
   e.preventDefault();
-  const data = new FormData(e.target);
+  const data = new FormData(e.target as HTMLFormElement);
   const value = Object.fromEntries(data.entries());
   return value;
 };

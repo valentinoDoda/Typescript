b906b951c89cc462db759ef0ad2169eafd12d45e
unknown <valododa123@gmail.com>
2025-08-05 18:54:42 +0200

    Fixing the errors without narrowing

--- annotations/code.ts
+++ annotations/code.ts
@@ -19,3 +19,21 @@ const handleFormData = (e: SubmitEvent) => {
   const value = Object.fromEntries(data.entries());
   return value;
 };
+
+type Color =
+  | string
+  | {
+      r: number;
+      g: number;
+      b: number;
+    };
+
+const config: Record<string, Color> = {
+  foreground: { r: 255, g: 255, b: 255 },
+  background: { r: 0, g: 0, b: 0 },
+  border: "transparent",
+};
+
+config.border.toUpperCase();
+
+console.log(config.foreground.r);

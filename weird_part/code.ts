9ee5376bc2062cd4df6e416c47f6b9c7a7e8e146
unknown <valododa123@gmail.com>
2025-08-06 11:11:00 +0200

    The problem

--- weird_part/code.ts
+++ weird_part/code.ts
@@ -13,3 +13,25 @@ acceptAnythingExceptNullOrUndefined(new Error("foo"));
 const acceptOnlyEmptyObject = (input: Record<PropertyKey, never>) => {};
 
 acceptOnlyEmptyObject({});
+
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const users = [
+  {
+    name: "Waqas",
+  },
+  {
+    name: "Zain",
+  },
+];
+
+const usersWithIds: User[] = users.map((user, index) => ({
+  ...user,
+  id: index,
+  // @ts-expect-error
+  age: 30,
+}));

7d268841442551103b4cf7994d1086ccbb32b9a0
unknown <valododa123@gmail.com>
2025-08-07 17:19:28 +0200

    Solution

--- Generics/code.ts
+++ Generics/code.ts
@@ -96,7 +96,7 @@ type BreadType = "rye" | "brown" | "white";
 
 type Filling = "cheese" | "ham" | "salami";
 
-type Sandwich = unknown;
+type Sandwich = `${BreadType} sandwich with ${Filling}`;
 
 type tests = [
   Expect<

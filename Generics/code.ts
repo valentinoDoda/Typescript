239a627a41dc66fb63fca050d7619c5c84a80b11
unknown <valododa123@gmail.com>
2025-08-07 17:20:01 +0200

    Delete some unsing code

--- Generics/code.ts
+++ Generics/code.ts
@@ -98,19 +98,3 @@ type Filling = "cheese" | "ham" | "salami";
 
 type Sandwich = `${BreadType} sandwich with ${Filling}`;
 
-type tests = [
-  Expect<
-    Equal<
-      Sandwich,
-      | "rye sandwich with cheese"
-      | "rye sandwich with ham"
-      | "rye sandwich with salami"
-      | "brown sandwich with cheese"
-      | "brown sandwich with ham"
-      | "brown sandwich with salami"
-      | "white sandwich with cheese"
-      | "white sandwich with ham"
-      | "white sandwich with salami"
-    >
-  >,
-];

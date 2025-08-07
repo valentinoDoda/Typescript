3bfd5253ef1bce8efb4296c2e9bb39749654b64b
unknown <valododa123@gmail.com>
2025-08-07 13:32:56 +0200

    Your task is to adjust the Result type so that TError defaults to Error.

--- Generics/code.ts
+++ Generics/code.ts
@@ -21,5 +21,34 @@ type PostDataShape = DataShape<{
   body: string;
 }>;
 
+type PromiseFunc<InputType, PromiseType> = (
+  input: InputType
+) => Promise<PromiseType>;
 
-type PromiseFunc<InputType, PromiseType> = (input: InputType) => Promise<PromiseType>;
+type Result<TResult, TError> =
+  | {
+      success: true;
+      data: TResult;
+    }
+  | {
+      success: false;
+      error: TError;
+    };
+
+const createRandomNumber = (): Result<number> => {
+  const num = Math.random();
+
+  if (num > 0.5) {
+    return {
+      success: true,
+      data: 123,
+    };
+  }
+
+  return {
+    success: false,
+    error: new Error("Something went wrong"),
+  };
+};
+
+const result = createRandomNumber();

fe55fdb646c08cdfd2c8c8634feffe60c61c0a9a
unknown <valododa123@gmail.com>
2025-08-08 09:03:29 +0200

    Your job is to transform createStringMap into a generic function capable of accepting a type argument for the values of the Map. Make sure it functions as expected based on the provided test cases!

---
+++ utils/code.ts
@@ -0,0 +1,31 @@
+
+
+// CODE
+
+const createStringMap = () => {
+  return new Map();
+};
+
+// TESTS
+
+const numberMap = createStringMap<number>();
+
+numberMap.set("foo", 123);
+numberMap.set(
+  "bar",
+  // @ts-expect-error
+  true,
+);
+
+const objMap = createStringMap<{ a: number }>();
+
+objMap.set("foo", { a: 123 });
+
+objMap.set(
+  "bar",
+  // @ts-expect-error
+  { b: 123 },
+);
+
+const unknownMap = createStringMap();
+
